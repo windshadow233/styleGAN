85c777ebf906f96b533906d62cfa3062d9648f6a
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-22 18:59:53 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+class EMA(object):
+    """
+    Exponential Moving Average
+    """
+    def __init__(self, mu):
+        self.mu = mu
+        self.shadow = {}
+
+    def register(self, name, val):
+        self.shadow[name] = val.clone()
+
+    def __call__(self, name, x):
+        assert name in self.shadow
+        new_average = self.mu * x + (1.0 - self.mu) * self.shadow[name]
+        self.shadow[name] = new_average.clone()
+        return new_average
