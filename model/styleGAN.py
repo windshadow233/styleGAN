d4871937be85a36f675ce443e0b5de28d10dfb2d
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-22 18:59:40 +0800

    Update styleGAN.py

--- model/styleGAN.py
+++ model/styleGAN.py
@@ -54,7 +54,8 @@ class SynthesisNet(nn.Module):
                  max_feature_maps=512,
                  use_leaky=True,
                  negative_slope=0.2,
-                 equalize_lr=True):
+                 equalize_lr=True,
+                 tanh_at_end=False):
         super(SynthesisNet, self).__init__()
         self.R = int(math.log2(max_resolution))
         assert 2 ** self.R == max_resolution
@@ -63,6 +64,7 @@ class SynthesisNet(nn.Module):
         self.feature_map_base = feature_map_base
         self.feature_map_decay = feature_map_decay
         self.max_feature_maps = max_feature_maps
+        self.tanh_at_end = tanh_at_end
         self.start_constant_tensor = nn.Parameter(torch.ones(size=(1, max_feature_maps, 4, 4)))
 
         """Network Structure"""
@@ -100,6 +102,8 @@ class SynthesisNet(nn.Module):
                     x = self.upsampler(x)
                 x = self.progressive_layers[i](x, w)
             x = self.to_rgb[to_ - 1](x)
+            if self.tanh_at_end:
+                x = torch.tanh(x)
             return x
         assert alpha is not None
         from_, to_ = 0, level - 2
@@ -111,6 +115,9 @@ class SynthesisNet(nn.Module):
         out1 = self.to_rgb[to_ - 1](out1)
         x = self.progress_growing[to_](x)
         out2 = self.to_rgb[to_](x)
+        if self.tanh_at_end:
+            out1 = torch.tanh(out1)
+            out2 = torch.tanh(out1)
         out = (1 - alpha) * out1 + alpha * out2
         return out
 
@@ -124,18 +131,22 @@ class Generator(nn.Module):
                  max_resolution=256,
                  feature_map_base=4096,
                  feature_map_decay=1,
-                 max_feature_maps=512,
+                 max_feature_maps=256,
                  use_leaky=True,
                  negative_slope=0.2,
                  equalize_lr=True,
+                 tanh_at_end=False,
                  **kwargs):
         super(Generator, self).__init__()
+        self.R = int(math.log2(max_resolution))
+        self.z_dim = z_dim
         self.mapping = MappingNet(z_dim, w_dim, normalize_latent,
                                   use_leaky=use_leaky, negative_slope=negative_slope,
                                   equalize_lr=equalize_lr, gain=2 ** 0.5)
         self.synthesis_net = SynthesisNet(num_channels, z_dim, w_dim, max_resolution,
                                           feature_map_base, feature_map_decay, max_feature_maps,
-                                          use_leaky=use_leaky, negative_slope=negative_slope, equalize_lr=equalize_lr)
+                                          use_leaky=use_leaky, negative_slope=negative_slope,
+                                          equalize_lr=equalize_lr, tanh_at_end=tanh_at_end)
 
     def forward(self, z, level, mode='stabilize', alpha=None):
         w = self.mapping(z)
